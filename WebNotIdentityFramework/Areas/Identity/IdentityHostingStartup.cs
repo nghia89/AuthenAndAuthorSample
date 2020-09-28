22285488a499aedc3c14d70875ff9a8db9bb6b90
NghiaNguyen <37038823+nghia89@users.noreply.github.com>
2020-09-28 21:13:14 +0700

    Scalffolded identity

---
+++ WebNotIdentityFramework/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using WebNotIdentityFramework.Data;
+
+[assembly: HostingStartup(typeof(WebNotIdentityFramework.Areas.Identity.IdentityHostingStartup))]
+namespace WebNotIdentityFramework.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<BookStoreDBContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("BookStoreDBContextConnection")));
+
+                services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<BookStoreDBContext>();
+            });
+        }
+    }
+}
